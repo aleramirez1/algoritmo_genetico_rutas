558fbb1667b6c61ae0878f4bb2577ed5edc921a3
Alexia <233380@ids.upchiapas.edu.mx>
2026-07-26 15:47:52 -0600

    certificado ssl

--- api/run_server.py
+++ api/run_server.py
@@ -14,4 +14,13 @@ if __name__ == "__main__":
     import uvicorn
     from app import app
 
-    uvicorn.run(app, host="0.0.0.0", port=8003)
+    ssl_cert = os.environ.get("SSL_CERT")
+    ssl_key = os.environ.get("SSL_KEY")
+
+    kwargs = {"host": "0.0.0.0", "port": 8003}
+
+    if ssl_cert and ssl_key:
+        kwargs["ssl_certfile"] = ssl_cert
+        kwargs["ssl_keyfile"] = ssl_key
+
+    uvicorn.run(app, **kwargs)
